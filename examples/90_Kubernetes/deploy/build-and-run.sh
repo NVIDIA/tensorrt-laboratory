4b777d8ef781e6f1e1e6ee7cae8fa856a2cdc44a
Ryan Olson <rolson@nvidia.com>
2018-07-16 07:13:39 -0600

    updated deployment bootstrap script

--- examples/90_Kubernetes/deploy/build-and-run.sh
+++ examples/90_Kubernetes/deploy/build-and-run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+default_engine=/work/models/ResNet-50-b1-fp32.engine
 concurrency=${YAIS_CONCURRENCY:-1}
+engine=${YAIS_TRT_ENGINE:-$default_engine}
 
-if [ ! -e /work/models/ResNet-50-b1-fp32.engine ]; then
-  cd /work/models
-  ./setup.py
+if [ "$engine" = "$default_engine" ]; then
+  if [ ! -e $engine ]; then
+    cd /work/models
+    ./setup.py
+  fi
 fi
 
 /work/build/examples/02_TensorRT_GRPC/inference-grpc.x \
-    --engine=/work/models/ResNet-50-b1-int8.engine \
+    --engine=${engine} \
     --contexts=${concurrency}
