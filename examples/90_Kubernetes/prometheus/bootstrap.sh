9490c95b10016f01319b31fd5c17c981d795a1a3
Ryan Olson <rolson@nvidia.com>
2018-08-20 03:38:03 -0700

    adding coreos-charts repo as part of the prometheus bootstrap

--- examples/90_Kubernetes/prometheus/bootstrap.sh
+++ examples/90_Kubernetes/prometheus/bootstrap.sh
@@ -4,6 +4,8 @@ kubectl create -f service-account.yml
 
 helm init --wait --service-account tiller
 
+helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
+
 helm install coreos/prometheus-operator \
   --name prometheus-operator \
   --namespace monitoring
