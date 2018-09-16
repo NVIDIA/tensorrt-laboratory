d5e151592e15c4f76c73bedc4782b585459018c6
Ryan Olson <rolson@nvidia.com>
2018-09-16 00:59:16 -0700

    basic config working

---
+++ examples/12_ConfigGenerator/link.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ln -s /work/build/examples/12_ConfigGenerator/config_generator.cpython-35m-x86_64-linux-gnu.so
--- examples/12_ConfigGenerator/test.py
+++
@@ -1,3 +0,0 @@
-import config_generator as cg
-
-print(cg.gen("flowers", "/work/model/flowers-152-b8-fp16.engine", 6))
