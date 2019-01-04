d5b4796d59baa6f611b907310f0f09145524d9fb
Ryan Olson <rolson@nvidiai.com>
2019-01-04 13:25:52 -0700

    updated py runtime example

--- examples/30_PyTensorRT/rebuild.sh
+++ examples/30_PyTensorRT/rebuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
-cd /work/build/examples/30_PyTensorRT
+cd /work/build/tensorrt-playground/python
 make
 cd /work/examples/30_PyTensorRT
+if [ ! -e infer.cpython-35m-x86_64-linux-gnu.so ]; then
+  ln -s /work/build/tensorrt-playground/python/tensorrt/infer.cpython-35m-x86_64-linux-gnu.so
+fi
