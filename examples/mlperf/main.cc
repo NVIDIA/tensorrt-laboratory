2a692d2ecb95c0ec5a28e3c974877bc553823bb5
Ryan Olson <rolson@nvidiai.com>
2019-01-25 19:56:40 -0700

    adding copyrights

--- examples/mlperf/main.cc
+++ examples/mlperf/main.cc
@@ -1,3 +1,30 @@
+/* Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 // Copyright 2018 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -62,6 +89,7 @@ int main(int argc, char*argv[])
     std::chrono::microseconds latency_bound(FLAGS_latency);
 
     // Batching timeout
+    volatile bool stop_batcher = false;
     constexpr uint64_t quanta = 100; // microseconds
     const double timeout = static_cast<double>(FLAGS_timeout - quanta) / 1000000.0; // microseconds
 
@@ -154,6 +182,7 @@ int main(int argc, char*argv[])
         // Batching Loop
         for (;;)
         {
+            if (unlikely(stop_batcher)) { return; }
             total_count = 0;
             max_deque = max_batch_size;
             adjustable_max_batch_size = max_batch_size;
@@ -260,5 +289,7 @@ int main(int argc, char*argv[])
               << std::roundl(100 * latency_bound_percentile) << "% latency bound: " << max_qps
               << "\n";
 
+    stop_batcher = true;
+    
     return 0;
 }
