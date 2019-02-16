dc5705f5986dc1c6bc08f9d74e88c017264b73af
Ryan Olson <rolson@nvidia.com>
2019-02-16 04:34:12 -0700

    clang-format

--- tensorrt-laboratory/nvrpc/include/nvrpc/executor.h
+++ tensorrt-laboratory/nvrpc/include/nvrpc/executor.h
@@ -38,8 +38,7 @@
 
 #include <glog/logging.h>
 
-namespace nvrpc
-{
+namespace nvrpc {
 
 class Executor : public IExecutor
 {
@@ -49,26 +48,27 @@ class Executor : public IExecutor
     Executor(std::unique_ptr<::trtlab::ThreadPool> threadpool);
     ~Executor() override {}
 
-    void Initialize(::grpc::ServerBuilder &builder) final override
+    void Initialize(::grpc::ServerBuilder& builder) final override
     {
-        for (int i = 0; i < m_ThreadPool->Size(); i++)
+        for(int i = 0; i < m_ThreadPool->Size(); i++)
         {
             m_ServerCompletionQueues.emplace_back(builder.AddCompletionQueue());
         }
     }
 
-    void RegisterContexts(IRPC *rpc, std::shared_ptr<::trtlab::Resources> resources, int numContextsPerThread) final override
+    void RegisterContexts(IRPC* rpc, std::shared_ptr<::trtlab::Resources> resources,
+                          int numContextsPerThread) final override
     {
-        auto base = dynamic_cast<IExecutor *>(this);
-        CHECK_EQ(m_ThreadPool->Size(), m_ServerCompletionQueues.size()) << "Incorrect number of CQs";
-        for (int i = 0; i < m_ThreadPool->Size(); i++)
+        auto base = dynamic_cast<IExecutor*>(this);
+        CHECK_EQ(m_ThreadPool->Size(), m_ServerCompletionQueues.size())
+            << "Incorrect number of CQs";
+        for(int i = 0; i < m_ThreadPool->Size(); i++)
         {
             auto cq = m_ServerCompletionQueues[i].get();
-            for (int j = 0; j < numContextsPerThread; j++)
+            for(int j = 0; j < numContextsPerThread; j++)
             {
                 DLOG(INFO) << "Creating Context " << j << " on thread " << i;
-                m_Contexts.emplace_back(
-                    this->CreateContext(rpc, cq, resources));
+                m_Contexts.emplace_back(this->CreateContext(rpc, cq, resources));
             }
         }
     }
@@ -77,25 +77,24 @@ class Executor : public IExecutor
     {
         for(auto& cq : m_ServerCompletionQueues)
         {
-            LOG(INFO) << "Telling CQ to Shutdown";
+            LOG(INFO) << "Telling CQ to Shutdown: " << cq.get();
             cq->Shutdown();
         }
-        exit(911);
+        // exit(911);
         LOG(INFO) << "Joining Executor Threads";
         m_ThreadPool.reset();
     }
+
     void Run() final override
     {
         // Launch the threads polling on their CQs
-        for (int i = 0; i < m_ThreadPool->Size(); i++)
+        for(int i = 0; i < m_ThreadPool->Size(); i++)
         {
-            m_ThreadPool->enqueue([this, i]{
-                ProgressEngine(i);
-            });
+            m_ThreadPool->enqueue([this, i] { ProgressEngine(i); });
             // m_Threads.emplace_back(&Executor::ProgressEngine, this, i);
         }
         // Queue the Execution Contexts in the recieve queue
-        for (int i = 0; i < m_Contexts.size(); i++)
+        for(int i = 0; i < m_Contexts.size(); i++)
         {
             // Reseting the context decrements the gauge
 #ifdef NVRPC_METRICS_ENABLED
